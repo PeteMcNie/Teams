ee437b7852e888b8d91a2c8074fc5a816e04b386
petemcnie <petemcnie0@gmail.com>
2020-06-20 07:47:16 +1200

    Addplayers component added

---
+++ client/components/Addplayers.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Addplayers = () => {
+    return (
+        <div>
+            <p>Add players here</p>
+
+            {/* Home button to be added here */}
+        </div>
+    )
+}
+
+export default Addplayers
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom'
 import Title from './title'
 import Links from './links'
 import CreateTeams from './createteams'
+import Addplayers from './addplayers'
 
 const App = () => {
   return (
     <>
       <Route path='/createteams' component={CreateTeams} />
+      <Route path='/addplayers' component={Addplayers} />
       <Route exact path='/' component={Title} />
       <Route exact path='/' component={Links} />
     </>
