eef3f3f60c98814cdfec6094d46e7276e21d8217
petemcnie <petemcnie0@gmail.com>
2020-06-20 07:26:40 +1200

    Title component created and linked

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Title from './title'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+      <Route path='/' component={Title} />
+    </>
   )
 }
 
