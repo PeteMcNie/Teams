6d7ef1babebe3b26e59ef1ba852cf9dc93120134
petemcnie <petemcnie0@gmail.com>
2020-06-23 08:46:38 +1200

    Server, routes and db js files created with code linking them all

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[environment]
+const database = require('knex')(config)
+
+module.exports = {
+    getPeople
+}
+
+function getPeople (db = database) {
+    return db('people').select()
+}
---
+++ server/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const db = require('./db')
+
+const router = express.Router()
+
+module.exporrts = router
+
+router.get('/createteams', (req, res) => {
+    db.getPeople()
+    .then(people => {
+        res.json(people)
+    })
+    .catch(err => {
+        res.status(500).send('Database error: ' + err.message)
+    })
+})
