f245f5614caee1d56419d609299c26e99dcaa79b
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-10 08:37:31 +1200

    Set up testing environment

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const path = require('path')
 
 module.exports = {
 
@@ -7,6 +8,20 @@ module.exports = {
     useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
+    }
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
     }
   },
 
--- server/validation/middleware.js
+++ server/validation/middleware.js
@@ -1,17 +1,17 @@
 const middleware = (schema) => {
-   console.log('schema: ', schema)
+   // console.log('schema: ', schema)
   return (req, res, next) => {
     const { error } = schema.validate(req.body)
     const valid = error == null
 
     if (valid) {
-      console.log('Valid')
+      // console.log('Valid')
       next()
     } else {
       const { details } = error
       const message = details.map(i => i.message).join(',')
 
-      console.log('Error', message)
+      // console.log('Error', message)
       res.status(422).json({ error: message })
     }
   }
