df4d7a133621f5682e5047ee2a7a5ab7f1857f31
petemcnie <petemcnie0@gmail.com>
2020-07-30 17:00:26 +1200

    Error message altered to disappear when ADD NEW PEOPLE SUCCESS reducer works

--- client/reducers/error.js
+++ client/reducers/error.js
@@ -1,10 +1,13 @@
-import { ERROR } from '../actions'
+import { ERROR, ADDING_NEW_PEOPLE } from '../actions'
 
 function errorMessage (error = null, action) {
     switch (action.type) {
         case ERROR:
             // return action.message OLD MESSAGE
             return ' Names entered must have at least two characters.'
+
+        case ADDING_NEW_PEOPLE:
+            return error = null
 
         default:
             return error
