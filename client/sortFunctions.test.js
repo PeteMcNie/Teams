4dd45c76d844c66bce4c2d1ebed972858da98711
petemcnie <petemcnie0@gmail.com>
2020-08-11 10:10:12 +1200

    Utils file created for frontend component tests

--- client/sortFunctions.test.js
+++ client/sortFunctions.test.js
@@ -1,7 +1,16 @@
 import { sortSelectedPeople } from './sortFunctions'
 
 test('sortSelectedPeople returns an object with two arrays inside', () => {
-  const object = [{ id: 1, name: 'bob' }, { id: 2, name: 'pete' }, { id: 3, name: 'sam' }, { id: 4, name: 'joe' }, { id: 5, name: 'kim' }, { id: 6, name: 'rikke' }, { id: 7, name: 'ari' }, { id: 8, name: 'emily' }]
+  const object = [
+    { id: 1, name: 'bob' }, 
+    { id: 2, name: 'pete' }, 
+    { id: 3, name: 'sam' }, 
+    { id: 4, name: 'joe' }, 
+    { id: 5, name: 'kim' }, 
+    { id: 6, name: 'rikke' }, 
+    { id: 7, name: 'ari' }, 
+    { id: 8, name: 'emily' }
+  ]
 
   // const expected = { teams: {
   //   teamOne: [{ id: 1, name: 'bob' }, { id: 2, name: 'pete' }, { id: 3, name: 'sam' }, { id: 4, name: 'joe' }],
@@ -9,7 +18,7 @@ test('sortSelectedPeople returns an object with two arrays inside', () => {
   // } }
 
   const expected = sortSelectedPeople(object)
-  // console.log('TEST: ', test)
+
   expect(expected.teams.teamOne).toHaveLength(4)
   expect(expected.teams.teamOne[1]).toHaveProperty('id')
 })
