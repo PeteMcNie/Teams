ae375f7800108c788062f4761fc2a68ad49ed557
petemcnie <petemcnie0@gmail.com>
2020-08-13 12:17:39 +1200

    Custom error messages depending on error thrown by joi middleware created

--- server/validation/schema.js
+++ server/validation/schema.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi')
 
 const schema = Joi.object({
   names: Joi.array().items(
-    Joi.string().alphanum().min(1).max(15).required()
+    Joi.string().pattern(new RegExp('^[a-zA-Z]{3,30}$')).min(1).max(15).required()
   )
 })
 
